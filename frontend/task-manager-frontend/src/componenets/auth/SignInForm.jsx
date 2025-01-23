f9dfa5ee7335bd756307fb68b1a27d721ad29f4b
nainu <ammadanwar416@gmail.com>
2025-01-23 12:54:33 +0500

    tailwind- landing page

--- frontend/task-manager-frontend/src/componenets/auth/SignInForm.jsx
+++ frontend/task-manager-frontend/src/componenets/auth/SignInForm.jsx
@@ -29,25 +29,30 @@ export default function SignInForm() {
     }
 
   return (
-    <div>
+    <div className="flex flex-col max-w-xs pb-8 w-full items-center bg-slate-600 rounded-xl gap-8 mt-5">
       <ToastContainer />
-      <h3>Welcome Back!</h3>
-      <form className="" onSubmit={handleLoginSubmit}>
-        <label className="">Email</label>
+      <h3 className="font-bold text-2xl font-serif text-black font-black ">Welcome Back!</h3>
+      <form className="flex flex-col border-blue-950 " onSubmit={handleLoginSubmit} >
+        <label className="text-sm font-bold">Email</label>
         <input 
         type="text"
+        className="bg-slate-800 placeholder:px-2 border border-black rounded-lg p-1 text-sm text-white"
         placeholder="example@gmail.com"
         required
         onChange={(e) => setEmail(e.target.value)}
          />
-        <label className="">Password</label>
+        <label className="text-sm mt-3 font-bold">Password</label>
         <input 
         type="text"
+        className="bg-slate-800 placeholder:px-2 border border-black rounded-lg p-1 text-sm text-white"
         placeholder="I am your Secret"
         required
         onChange={(e) => setPassword(e.target.value)}
          />
-         <button type="submit" onClick={() => toast.success("Please Wait!")}>
+         <button 
+         className="bg-slate-50 w-full rounded-xl text-sm py-1 dark:text-black my-3"
+         type="submit" 
+         onClick={() => toast.success("Please Wait!")}>
             Sign In
          </button>
       </form>
--- frontend/task-manager-frontend/src/pages/Login.jsx
+++ frontend/task-manager-frontend/src/pages/Login.jsx
@@ -9,19 +9,21 @@ const Login = () => {
     navigate('/signup')
   }
   return (
-    <div>
-      <h2>The Ultimate Task Manager</h2>
-      <div>
+    <div className='flex flex-col'>
+      <h2 className='font-bold text-3xl font-serif text-blue-950 font-black'>The Ultimate Task Manager</h2>
+      <div className='flex-grow flex-col flex items-center justify-center w-full'>
         <SignInForm />
-        <p>
+        <p className='pt-2 font-semibold'>
           No Account yet?
         </p>
-        <button onClick={handleClick}>SignUp</button>
+        <button onClick={handleClick} className='bg-slate-900 px-8 py-2 rounded-3xl my-3 hover:font-semibold text-white'>
+          Sign Up
+        </button>
       </div>
             {/* just for the sake of testing */}
-      <button onClick={() => navigate('/task')}>
+      {/* <button onClick={() => navigate('/task')} >
         linking to task page for testing 
-      </button>
+      </button> */}
     </div>
   )
 }
